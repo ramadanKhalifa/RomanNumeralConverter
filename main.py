7edc43fe4469af02f0ff3155e789e7a8d225b917
Ramadan Khalifa <49395401+ramadanKhalifa@users.noreply.github.com>
2020-02-28 13:50:11 +0100

    Update main.py
    add int_to_roman func

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 def int_to_roman(num: int) -> str:
-    pass
+    """
+    convert an integer number to a Roman numeral.
+    :param num: integer number between 1:3999 inclusive.
+    :return: Roman numeral (string).
+    """
+    if num < 1 or num > 3999:
+        raise ValueError("Invalid input:({})! Roman numeral is only valid between 1 and 3999.".format(num))
+    int_roman_pairs = (
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I'),
+    )
+    result = ''
+    for int_value, roman_char in int_roman_pairs:
+        symbols_count = num // int_value
+        if symbols_count == 0:
+            continue
+        result += roman_char * symbols_count
+        num -= int_value * symbols_count
+    return result
 
 
 assert int_to_roman(2002) == "MMII"
